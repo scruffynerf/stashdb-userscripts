26cf60bf75fbdf0f30797ed7d35b0bf9df429198
stg-annon <14135675+stg-annon@users.noreply.github.com>
2023-09-17 19:21:21 -0400

    build.py

--- dist/public/StashDB Userscripts Bundle.user.js
+++ dist/public/StashDB Userscripts Bundle.user.js
@@ -5,7 +5,7 @@
 // @version     0.3.0
 // @author      7dJx1qP
 // @match       https://stashdb.org/*
-// @resource    IMPORTED_CSS https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/master/dist/public/scene.css
+// @resource    IMPORTED_CSS https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/main/dist/public/scene.css
 // @grant       unsafeWindow
 // @grant       GM_setClipboard
 // @grant       GM_getResourceText
@@ -14,12 +14,12 @@
 // @grant       GM.setValue
 // @grant       GM.listValues
 // @grant       GM.xmlHttpRequest
-// @require     https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/master/src\StashDBUserscriptLibrary.js
+// @require     https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/main/src\StashDBUserscriptLibrary.js
 //
 // **************************************************************************************************
 // *            YOU MAY REMOVE ANY OF THE @require LINES BELOW FOR SCRIPTS YOU DO NOT WANT          *
 // **************************************************************************************************
 //
-// @require     https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/master/src/body\StashDB Copy StashID.user.js
+// @require     https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/main/src/body\StashDB Copy StashID.user.js
 
 // ==/UserScript==
