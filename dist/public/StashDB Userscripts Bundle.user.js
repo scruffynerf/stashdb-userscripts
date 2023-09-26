638e640c23dc93cd2cd8e6bd340a5b3a1f8b68ea
stg-annon <14135675+stg-annon@users.noreply.github.com>
2023-09-26 15:06:47 -0400

    re-target repo

--- dist/public/StashDB Userscripts Bundle.user.js
+++ dist/public/StashDB Userscripts Bundle.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        StashDB Userscripts Bundle
-// @namespace   https://github.com/7dJx1qP/stashdb-userscripts
+// @namespace   https://github.com/stg-annon/stashdb-userscripts
 // @description StashDB Userscripts Bundle
 // @version     0.3.0
 // @author      7dJx1qP
@@ -14,12 +14,12 @@
 // @grant       GM.setValue
 // @grant       GM.listValues
 // @grant       GM.xmlHttpRequest
-// @require     https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/main/src\StashDBUserscriptLibrary.js
+// @require     https://raw.githubusercontent.com/stg-annon/stashdb-userscripts/main/src\StashDBUserscriptLibrary.js
 //
 // **************************************************************************************************
 // *            YOU MAY REMOVE ANY OF THE @require LINES BELOW FOR SCRIPTS YOU DO NOT WANT          *
 // **************************************************************************************************
 //
-// @require     https://raw.githubusercontent.com/7dJx1qP/stashdb-userscripts/main/src/body\StashDB Copy StashID.user.js
+// @require     https://raw.githubusercontent.com/stg-annon/stashdb-userscripts/main/src/body\StashDB Copy StashID.user.js
 
 // ==/UserScript==
